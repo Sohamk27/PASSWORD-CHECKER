cf140f5393943f7642fa97f108258db9fb66254a
Soham Kundu <77265074+Sohamk27@users.noreply.github.com>
2021-05-27 16:47:12 +0530

    Create password.py
    This code will accept input from command line also. You can input all the passwords that are in your mind with a space in between and you will get to know which password you should choose.

---
+++ password.py
@@ -0,0 +1,48 @@
+import requests
+import hashlib
+import sys
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+    return res
+
+def read(answer, tail):
+    sen = answer.text
+    # print(sen)
+    li = sen.split()
+    password = li
+    j = 0
+    for i in password:
+        tup = tuple(i.split(':'))
+        if(tup[0] == tail):
+            print('not safe')
+            print(f'password has been hacked {tup[1]} times')
+            print(i)
+            j = 1
+            break
+
+    if j == 0 :
+        print('good to go') 
+
+
+def pwnew_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    #print(sha1password)
+    firstfive, tail = sha1password[0:5], sha1password[5:]
+    #print(firstfive)
+    answer = request_api_data(firstfive)
+    print(answer)
+    read(answer, tail)
+       
+    
+# password = list(input('enter passwrod to be checked\n').split())
+# for x in password:
+#     pwnew_api_check(x)
+
+pw = sys.argv[1:]
+for item in pw:
+    pwnew_api_check(item)
